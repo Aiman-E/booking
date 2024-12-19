99b0287030db6057a8d012ab87008f8dd08165c2
Aiman_E <eryani.a.man@gmail.com>
2024-12-19 16:19:31 +0000

    initial commit

--- booking/booking/doctype/reservation/reservation.js
+++ booking/booking/doctype/reservation/reservation.js
@@ -8,26 +8,139 @@
 // });
 
 
-frappe.ui.form.on('Reservation', {
-    onload: function(frm) {
-        // Code to run when the form is loaded
-        if (frm.is_new()) {
-            // Check if start_date and end_date are set
-        if (frm.doc.start_date && frm.doc.end_date) {
-            // Create a Date object from start_date
-            let startDate = new Date(frm.doc.start_date);
-            let endDate = new Date(frm.doc.end_date);
-            
-            // Add 2 hours and 30 minutes to start_date
-            startDate.setHours(startDate.getHours() + 2);
-            startDate.setMinutes(startDate.getMinutes() + 30);
-            frm.set_value('start_date', frappe.datetime.convert_to_user_tz(startDate));
+function calculate_grand_total(frm) {
+    frm.doc.grand_total = 0;
+    frm.doc.items.forEach(i => {
+        frm.doc.grand_total += i.rate * frm.doc.duration;
+    });
+}
 
-            // Add 2 hours and 30 minutes to end_date
-            endDate.setHours(endDate.getHours() + 2);
-            endDate.setMinutes(endDate.getMinutes() + 30);
-            frm.set_value('end_date', frappe.datetime.convert_to_user_tz(endDate));
+frappe.ui.form.on('Reservation', {
+    onload: function (frm) {
+        // Add two and half hours since the code doesn't include the local time
+        if (frm.is_new()) {
+            if (frm.doc.start_date && frm.doc.end_date) {
+                // Create a Date object from start_date
+                let startDate = new Date(frm.doc.start_date);
+                let endDate = new Date(frm.doc.end_date);
+
+                // Add 2 hours and 30 minutes to start_date
+                startDate.setHours(startDate.getHours() + 2);
+                startDate.setMinutes(startDate.getMinutes() + 30);
+                frm.set_value('start_date', frappe.datetime.convert_to_user_tz(startDate));
+
+                // Add 2 hours and 30 minutes to end_date
+                endDate.setHours(endDate.getHours() + 2);
+                endDate.setMinutes(endDate.getMinutes() + 30);
+                frm.set_value('end_date', frappe.datetime.convert_to_user_tz(endDate));
+
+                frm.trigger("calculate_duration");
+            }
         }
+    },
+    refresh: function (frm) {
+        if (frm.doc.docstatus == 0 && !frm.is_new()  ) {
+            frm.add_custom_button(__("Generate Invoice"),
+                () => frm.trigger("generate_invoice"));
+
+            frm.add_custom_button(__("Cancel Reservation"),
+                () => frm.trigger("cancel_invoice"));
+
+            frm.add_custom_button(__("Update Reservation Status"),
+                () => frm.trigger("update_status"));
+
+            frm.add_custom_button(__("Create Repeats"),
+                () => frm.trigger("create_repeats"));
+            
         }
+    },
+    start_date: function (frm) {
+        frm.trigger("calculate_duration");
+    },
+    end_date: function (frm) {
+        frm.trigger("calculate_duration");
+    },
+    duration: function (frm) {
+        frm.trigger("calculate_grand_total");
+    },
+    generate_invoice: function (frm) {
+        frappe.confirm(__("Are you sure you want to generate an invoice?"), () => {
+            frm.call("generate_invoice").then((r) => {
+                if (!r.exec) {
+                    frm.reload_doc();
+                }
+            });
+        });
+    },
+    cancel_invoice: function (frm) {
+        frappe.confirm(__("Are you sure you want to cancel the invoice?"), () => {
+            frm.call("cancel_invoice").then((r) => {
+                if (!r.exec) {
+                    frm.reload_doc();
+                } else {
+                    // Handle the case where an exception was raised
+                    frappe.show_alert({
+                        message: r.exc,
+                        indicator: 'red'
+                    });
+                }
+            });
+        });
+    },
+    calculate_duration: function(frm) {
+        frm.call("calculate_duration").then((r) =>{
+            if(!r.exec){
+                calculate_grand_total(frm);
+                frm.refresh_fields();
+            }  
+        })
+    },
+    update_status: async function(frm) {
+        await frm.reload_doc();
+        frm.call("update_status").then((r) => {
+            if (!r.exec) {
+                frm.refresh_fields();
+                frm.reload_doc();
+            }
+        })
+    },
+    create_repeats: function(frm) {
+        frm.call("create_repeats").then((r) => {
+            if (!r.exec) {
+                frm.refresh_fields();
+            }
+        })
+    }
+});
+
+frappe.ui.form.on('Reservation Item', {
+    items_add: function (frm, cdt, cdn) {
+        
+    },
+    item_code: function(frm, cdt, cdn) {
+        row = locals[cdt][cdn]
+        frm.call(
+            'update_item_rates',
+            {"cdt": cdt, "cdn": cdn, "item_code": row.item_code}
+        ).then((r)=>{
+            if(!r.exec){
+                rate = r['message']['rate'];
+                cdt = r['message']['cdt'];
+                cdn = r['message']['cdn'];
+
+                row = locals[cdt][cdn];
+                row.rate = rate;
+
+                calculate_grand_total(frm);
+
+                frm.refresh_fields();
+            } else {
+                // Handle the case where an exception was raised
+                frappe.show_alert({
+                    message: r.exc,
+                    indicator: 'red'
+                });
+            }
+        });
     }
 });
--- booking/booking/doctype/reservation/reservation.py
+++ booking/booking/doctype/reservation/reservation.py
@@ -5,17 +5,224 @@ import json
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
 
+from erpnext import get_default_company, get_default_cost_center
+from erpnext.accounts.doctype.accounting_dimension.accounting_dimension import (
+	get_accounting_dimensions,
+)
 
-class Reservation(Document):	
+from datetime import datetime, timedelta
 
-	def before_insert(self):
-		a=1
+'''
+	TODO: Export Customization from sales invoice
+	TODO: Double check on conditions and validations
+	TODO: Retrict saving price rate if existed or not
+	TODO: multiple dates with same time
+	TODO: check if end before start date and raise error
+'''
+class Reservation(Document):
+	def validate(self):
+		# check the price rate if existed or not
 		pass
 
-	def before_save(self):
-		a=1
+	def get_items(self):
+		items = []
+		
+		for i in self.items:
+			item = {
+					"item_code": i.item_code,
+					"rate": i.rate,
+					"qty": self.duration,
+				}
+			items.append(item)
+
+		return items
+	
+	def check_dates(self):
 		pass
+
+	def set_status(self, status):
+		self.status = status
+
+	def calculate_total_hours(self):
+		whole_hours = int(self.duration)
+		minutes = int((self.duration - whole_hours) * 60)
+		self.total_hours = f"{whole_hours} hours - {minutes} minutes"
+
+	def get_month_in_days(self, month):
+		if month == 2: return 28
+		if month in [1, 3, 5, 7,8, 10, 12]: return 31
+		return 30
+
+	@frappe.whitelist()
+	def update_status(self):
+		if self.status == "Cancelled and Refunded":
+			return
+		
+		self.status = "Unconfirmed"
+
+		linked_invoices = frappe.get_all('Sales Invoice',
+									fields=[
+										'name',
+										'status',
+										'docstatus'
+									],
+									filters={
+										'custom_reservation': self.name,
+		})
+
+		if len(linked_invoices) > 0:
+			i = linked_invoices[0] # Get latest invoice
+			if i['docstatus'] == 1:
+				self.status = "Confirmed"
+
+			if i['docstatus'] == 2:
+				self.status = "Cancelled"
+
+		self.save()
+			
+				
+
+	@frappe.whitelist()	
+	def generate_invoice(self):
+		linked_invoices = frappe.get_all('Sales Invoice',
+								   fields=[
+									   'name',
+									   'status'
+								   ],
+								   filters={
+									   'custom_reservation': self.name,
+        })
+
+		for i in linked_invoices:
+			if i['status'] == "Cancelled":
+				continue
+			frappe.throw(
+				_(
+					"Sales invoice already exist"
+				)
+			)
+
+		company = get_default_company()
+		cost_center = get_default_cost_center(company)
+
+		if not company:
+			frappe.throw(
+				_(
+					"Company is mandatory for generating an invoice. Please set a default company in Global Defaults."
+				)
+			)
+		
+		invoice = frappe.new_doc("Sales Invoice")
+		invoice.company = company
+
+		invoice.cost_center = cost_center
+		invoice.customer = self.customer
+
+		item_list = self.get_items()
+		for item in item_list:
+			item['cost_center'] = cost_center
+			invoice.append("items", item)
+
+		self.save()
+		invoice.custom_reservation = self.name
+
+		if(self.paid_amount > 0):
+			invoice.is_pos = 1
+			payment = {
+				"mode_of_payment": self.mode_of_payment,
+				"amount": self.paid_amount
+			}
+			invoice.append("payments", payment)
+
+		invoice.insert()
+		invoice.submit()
+
+	@frappe.whitelist()
+	def cancel_invoice(self):
+		linked_invoices = frappe.get_all('Sales Invoice',
+								   filters={
+									   'custom_reservation': self.name
+        })
+
+		if linked_invoices:
+			for invoice in linked_invoices:
+				invoice_doc = frappe.get_doc('Sales Invoice', invoice.name)
+				
+				# Check if the invoice is already cancelled
+				if invoice_doc.docstatus == 2:
+					frappe.msgprint(_(f"Invoice {invoice.name} is already cancelled."))
+					continue
+				
+				# Cancel the invoice
+				invoice_doc.cancel()
+				frappe.msgprint(_(f"Sales Invoice {invoice.name} has been cancelled."))
+		else:
+			frappe.msgprint_(("No linked invoice found for this reservation."))
+		
+
+	@frappe.whitelist()
+	def update_item_rates(self, args):
+		rate = frappe.get_list('Item Price', ['price_list_rate'], {'item_code': args['item_code']})
+		if len(rate) > 0:
+			return {"cdt": args['cdt'], "cdn": args['cdn'], "rate": rate[0]['price_list_rate']}
+		
+		frappe.throw(_(f"Create price rate for item '{args['item_code']}' before using it"))
+
+	@frappe.whitelist()
+	def calculate_duration(self):
+		if self.start_date and self.end_date:
+			# Calculate the difference
+			start = datetime.fromisoformat(self.start_date)
+			end = datetime.fromisoformat(self.end_date)
+			duration = (end - start).total_seconds() / 3600
+			self.duration = duration
+
+			self.calculate_total_hours()		
+
+	@frappe.whitelist()
+	def create_repeats(self):
+		start_date = datetime.fromisoformat(self.start_date)
+		end_date = datetime.fromisoformat(self.end_date)
+		interval = self.interval
+		count = self.count
+
+		for i in range(count):
+			if interval == "Daily":
+				start_date += timedelta(days=1)
+				end_date += timedelta(days=1)
+			elif interval == "Weekly":
+				start_date += timedelta(days=7)
+				end_date += timedelta(days=7)
+			elif interval == "Monthly":
+				d = self.get_month_in_days(start_date.month)
+				start_date += timedelta(days=d)
+				end_date += timedelta(days=d)
+
+			new_reservation = frappe.get_doc({
+				"doctype": "Reservation",
+				"start_date": start_date.isoformat(),
+				"end_date": end_date.isoformat(),
+				"customer": self.customer,
+				"items": self.items,
+				"confirmation": self.confirmation,
+				"paid_amount": self.paid_amount,
+				"mode_of_payment": self.mode_of_payment,
+				"receipt_number": self.receipt_number,
+				"grand_total": self.grand_total,
+				"is_repeat_reservation": 1,
+				"parent_reservation": self.name,
+			})
+			new_reservation.insert()
+
+			self.append("linked_reservations", {
+				"reservation": new_reservation.name,
+				"status": "Unconfirmed"
+			})
+
+			
+		self.save()
 
 
 """select `tabTimesheet Detail`.name as name,
@@ -33,16 +240,17 @@ class Reservation(Document):
 def get_events(start, end, filters=None):
 
 	d = frappe.db.sql("""
-		select
+		SELECT
 			name,
 			title,
 			start_date,
 			end_date
-		from `tabReservation`
+		FROM `tabReservation`
+		WHERE status != 'Cancelled'
 		""",
 		as_dict=True,
 		update= {
-		'allDay': 0,
+		'allDay': 0
 	},
 	)
 
